03d82b044c6d47dab3b1cc4a40b5335680f3a763
druidafox <vitorhugosjrp@gmail.com>
2022-07-30 13:16:18 -0300

    feat: initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+// vamos começar, carregando o express e criando a app
+const app = require('express')()
+const path = require('path')
+
+// então, criamos uma rota para '/'
+app.get('/', (req, res) => {
+  // aqui precisamos enviar o index.html para o cliente
+  res.sendFile(path.join(__dirname + '/formulario.html'))
+})
+app.post('/resultado', (req, res) => {
+    // aqui precisamos enviar o index.html para o cliente
+    res.sendFile(path.join(__dirname + '/resultado.html'))
+})
+    app.listen(8001)
+console.log('Servidor rodando na porta 8001!')
