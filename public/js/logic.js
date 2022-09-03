ee1657b49e8950019dfea0ede9e475ae015de0cc
druidafox <vitorhugosjrp@gmail.com>
2022-09-03 12:01:38 -0300

    tabelaassync

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,3 +1,5 @@
+
+
 /**
  * Return true if is valid email
  * @returns boolean
@@ -36,23 +38,30 @@ function capitalize(filedObject) {
  * TODO fazer a rotina de limpar as mesagens de errors
  * @returns a object with data of form and verifies if exists errors
  */
-function createFormData() {
-    var error = false;
-    var name = $("#name").val();
-    if (name === "") {
-        error = true;
-        $("#nameMessage").text("Nome não pode ser vazio");
-    }
-    var lastName = $("#lastName").val();
-    var phone = $("#phone").val();
-    var email = $("#email").val();
-    var age = $("#age").val();
 
-    var formData = { name, lastName, phone, email, age, error }
 
-    return formData;
+function serializeForm(form) {
+    var hasError = false;
+    const data = form.serializeArray();
+    var fieldErros = [];
+    $.each(data, (index, field) => {
+        field.value = $.trim(field.value);
+        if (!field.value) {
+            hasError = true;
+            fieldErros.push(field.name);
+        }
+    })
+    return { hasError, data, fieldErros };
 }
 
+function showErrorMessages(fieldErros) {
+    $.each(fieldErros, (index, field) => {
+        var span = $("span[name=" + field + "ErrorMessage]")
+        span.text("Este campo não pode ser vazio")
+    })
+}
+
+const tableBody = document.querySelector("#table > tbody");
 /**
  * TODO criar um objeto para concentrar as funções de users
  * TODO tonar as chamadas agnósticas
@@ -60,19 +69,43 @@ function createFormData() {
  */
 function send() {
 
-    var data = createFormData();
-
-    if (data.error) {
+    var data = serializeForm($("#formcad"));
+    var datausers = data.data;
+    var arraydata = [];
+    if (data.hasError) {
+        showErrorMessages(data.fieldErros)
         return;
     }
 
     $.ajax({
         url: "/users",
         type: "POST",
-        data: data,
-        success: function (result) {
-            console.log(result);
-            $('p').append(JSON.stringify(result) + "<br/>");
+        data: datausers,
+        success: function (sucess) {
+            datausers.forEach(function (value) {
+                arraydata.push(value.value);
+            });
+            const tbody = $('#table').children('tbody');
+            const table = document.getElementById('table');
+            const tr = document.createElement('tr');
+            arraydata.forEach(function (value, key) {
+                const td = document.createElement('td');
+                const td2 = document.createElement('td');
+                const td3 = document.createElement('td');
+
+                const name = document.createTextNode(value);
+               td.append(name);
+               console.log(td);
+               tr.append(td);
+               tbody.append(tr);
+
+            
+
+               
+
+
+
+            });
         },
         error: function (jqXhr, textStatus, errorMessage) {
             console.error(errorMessage);
@@ -80,3 +113,12 @@ function send() {
     });
 }
 
+
+function cleartable() {
+    while (tableBody.firstChild) { //enquanto a tabela tiver qualquer conteúdo dentro do tbody durante o carregamento ele irá deletar.//
+        tableBody.removeChild(tableBody.firstChild);
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", () => { cleartable(); }); //executa os scprits após o do carregamento completo da página
--- server.js
+++ server.js
@@ -1,28 +1,49 @@
 const app = require('express')();
 const express = require('express');
 const path = require('path');
-
+const bodyParser = require('body-parser')
+const users = require("./users");
+const { response, json } = require('express');
+// create application/json parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+ 
 /**
  * criar um id randomico para cada novo usuário
  */
 const database = [];
+const banco = {};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/bienevindo.html'));
+  res.status(200).json;
+  
+});
+
+app.get('/formulario', (req, res) => {
   res.sendFile(path.join(__dirname + '/form.html'));
 });
 
-app.post('/users', (req, res) => {
-  console.log("adding user: " + JSON.stringify(req.body));
-  res.status(200).json({ result: "ok" });
+app.post('/users',function(req, res){
+var json = (req.body);
+const myJSON =(users);
+res.status(200).json(users);
+ 
+  
+  //console.log("adding user: " + JSON.stringify(req.body));
+  //res.status(200).json({result:database});
+  //const myJSON = JSON.parse(users)
+ // console.log(myJSON);
 });
 
 app.get('/users', (req, res) => {
-  console.log("listing " + database.length + " users");
-  res.status(200).json({ users: database });
+  console.log("listing " + users.length + " users");
+  
+  res.status(200).json({ database });
 });
 
 app.listen(8001);
